b68987e1c57a1dbc2861b4c2215301069bbf86ab
Namanj71 <naman71@gmail.com>
2017-08-10 00:37:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,11 @@ element
 //to change value as
 element.innerHTML="Hello to my first demo";
 //to call image 
-var eleimg=document.getElementById("img");
+var img=document.getElementById("madi");
 var marginleft=0;
 function moveRight(){
-    marginleft=marginleft+10;
-    eleimg.style.marginleft=marginleft+'px';
+    marginleft=marginleft+'10px';
 }
-eleimg.onclick=function(){
+img.onclick=function(){
   var interval=setInterval(moveRight,50);  
 };
