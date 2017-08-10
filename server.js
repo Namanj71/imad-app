035d93f2152341514444cd9c4426c5e2ab99dd78
Namanj71 <naman71@gmail.com>
2017-08-10 23:16:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
